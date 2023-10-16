f4cd35569a221b09b65118998b17b299fb1b32ec
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-10-16 22:04:55 +0900

    add marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,13 @@
-const TestPage = () => {
-  return <div>Hello Test Page</div>;
+import { Heading } from "./_components/Heading";
+import { Heroes } from "./_components/Heroes";
+
+const MarketingPage = () => {
+  return <div className="min-h-full flex flex-col">
+    <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+      <Heading />
+      <Heroes />
+    </div>
+  </div>;
 };
 
-export default TestPage;
+export default MarketingPage;
