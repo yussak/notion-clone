d01dd58fa8c82e9906da789f3625e0979c8485ff
yussak <yusuke.sakuraba0213@gmail.com>
2025-11-22 19:38:00 +0900

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from "cors";
 const app = express();
 const PORT = 8080;
 
+// TODO: cors設定
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
