6b1077946a2e12cb48b0265ce6230e7a62102d54
yussak <yusuke.sakuraba0213@gmail.com>
2025-11-22 20:26:15 +0900

    refactor

--- backend/src/controllers/pageController.ts
+++ backend/src/controllers/pageController.ts
@@ -1,15 +1,12 @@
-import { Request, Response } from 'express';
-import client = require('../generated/prisma/client');
+import { Request, Response } from "express";
+import client = require("../generated/prisma/client");
 
 const prisma = new client.PrismaClient();
 
 export const createPage = async (req: Request, res: Response) => {
-  const a = await prisma.page.create({
-    data: {}
+  const page = await prisma.page.create({
+    data: {},
   });
 
-  const createdId = a.id;
-
-  res.json({ id: createdId });
+  res.json({ id: page.id });
 };
-
