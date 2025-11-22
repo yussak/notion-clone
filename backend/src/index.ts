cd4c4ea747f051c2149183c0f6e9861bdc1fa917
yussak <yusuke.sakuraba0213@gmail.com>
2025-11-22 20:27:43 +0900

    remove unused router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import pagesRouter from './routes/pageRoutes';
+import pagesRouter from "./routes/pageRoutes";
 
 const app = express();
 const PORT = 8080;
@@ -9,11 +9,7 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', pagesRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use("/api", pagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
