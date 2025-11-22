486a2fa5202476d2f2ddb0a4c32c30d4185775ba
yussak <yusuke.sakuraba0213@gmail.com>
2025-11-22 20:26:27 +0900

    format file

--- frontend/app/pages/[id]/page.tsx
+++ frontend/app/pages/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default async function Page({params}) {
-  const {id} = await params;
+export default async function Page({ params }) {
+  const { id } = await params;
   return <div>{id}</div>;
 }
