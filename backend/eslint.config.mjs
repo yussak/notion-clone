abd8e2a11017314dc54798226b58a9c6439cffdd
yussak <yusuke.sakuraba0213@gmail.com>
2025-11-29 11:33:08 +0900

    update eslint setting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,5 +1,14 @@
+import tseslint from "typescript-eslint";
+
 export default [
   {
     ignores: ["src/generated/**"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      // 未使用変数の禁止
+      "no-unused-vars": "error",
+    },
   },
 ];
